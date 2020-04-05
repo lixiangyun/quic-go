20f51e2e5e6d8dde34c476f3b932795910504e93
lixiangyun <linimbus@126.com>
2020-04-05 17:15:31 +0800

    add demo

--- example/echo/echo.go
+++ example/echo/echo.go
@@ -1,76 +1,158 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
+	"flag"
 	"io"
 	"log"
 	"math/big"
+	"time"
 
 	quic "github.com/lucas-clemente/quic-go"
 )
 
-const addr = "localhost:4242"
+var (
+	Address string
+	Role    string
+	Message int
+	Par     int
 
-const message = "foobar"
+	Help    bool
 
-// We start a server echoing data on the first stream the client opens,
-// then connect with a client, send the message, and wait for its receipt.
+	message []byte
+)
+
+func init()  {
+	flag.StringVar(&Address,"addr","0.0.0.0:1000","listen udp port")
+	flag.IntVar(&Message,"msg",1024,"message size len")
+	flag.StringVar(&Role,"role","server","server/client")
+	flag.IntVar(&Par,"par",10,"par message for send/recv")
+
+	flag.BoolVar(&Help,"help",false,"usage help")
+}
+
 func main() {
-	go func() { log.Fatal(echoServer()) }()
 
-	err := clientMain()
-	if err != nil {
-		panic(err)
+	flag.Parse()
+	if Help {
+		flag.Usage()
+		return
+	}
+
+	if Role == "server" {
+		log.Printf("quic server start")
+		log.Fatal(Server())
+		return
+	}
+
+	message = make([]byte, Message)
+	for i:=0;i<Message;i++{
+		message[i] = byte('a')
+	}
+
+	if Role == "client" {
+		log.Printf("quic client start")
+		Client()
 	}
 }
 
 // Start a server that echos all data on the first stream opened by the client
-func echoServer() error {
-	listener, err := quic.ListenAddr(addr, generateTLSConfig(), nil)
+func Server() error {
+	listener, err := quic.ListenAddr(Address, generateTLSConfig(), nil)
 	if err != nil {
 		return err
 	}
-	sess, err := listener.Accept()
-	if err != nil {
-		return err
+	for {
+		sess, err := listener.Accept(context.Background())
+		if err != nil {
+			log.Printf("listen accept fail %s", err.Error())
+			continue
+		}
+
+		log.Printf("accept session %s", sess.RemoteAddr())
+		go func() {
+			for {
+				stream, err := sess.AcceptStream(context.Background())
+				if err != nil {
+					log.Printf("session accept stream %s", err.Error())
+					return
+				}
+
+				log.Printf("new stream success: %d", stream.StreamID())
+
+				go func() {
+
+					bufer := make([]byte,8192)
+					for {
+						cnt,err := stream.Read(bufer)
+						if err != nil {
+							log.Printf("stream read fail:%s",err.Error())
+							break
+						}
+
+						StatAdd(cnt)
+
+						_, err = stream.Write(bufer[:cnt])
+						if err != nil {
+							log.Printf("stream write fail:%s",err.Error())
+							break
+						}
+					}
+
+				}()
+			}
+		}()
 	}
-	stream, err := sess.AcceptStream()
-	if err != nil {
-		panic(err)
-	}
-	// Echo through the loggingWriter
-	_, err = io.Copy(loggingWriter{stream}, stream)
-	return err
 }
 
-func clientMain() error {
-	session, err := quic.DialAddr(addr, &tls.Config{InsecureSkipVerify: true}, nil)
+func Client() error {
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+	session, err := quic.DialAddr(Address, tlsConf, nil)
 	if err != nil {
 		return err
 	}
 
-	stream, err := session.OpenStreamSync()
-	if err != nil {
-		return err
+	for i:=0; i<Par ; i++ {
+
+		go func() {
+			stream, err := session.OpenStreamSync(context.Background())
+			if err != nil {
+				log.Printf("open stream fail : %s", err.Error())
+				return
+			}
+
+			recvBuf := make([]byte,8192)
+			var writeCnt int
+			var readCnt int
+
+			log.Printf("new stream success %d", stream.StreamID())
+
+			for {
+				writeCnt, err = stream.Write([]byte(message))
+				if err != nil {
+					log.Printf("stream write fail : %s", err.Error())
+					return
+				}
+				readCnt, err = stream.Read(recvBuf)
+				if err != nil {
+					log.Printf("stream read fail : %s", err.Error())
+					return
+				}
+				StatAdd(readCnt+writeCnt)
+			}
+		}()
+
 	}
 
-	fmt.Printf("Client: Sending '%s'\n", message)
-	_, err = stream.Write([]byte(message))
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, len(message))
-	_, err = io.ReadFull(stream, buf)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Client: Got '%s'\n", buf)
+	time.Sleep(3600*time.Second)
 
 	return nil
 }
@@ -79,7 +161,6 @@ func clientMain() error {
 type loggingWriter struct{ io.Writer }
 
 func (w loggingWriter) Write(b []byte) (int, error) {
-	fmt.Printf("Server: Got '%s'\n", string(b))
 	return w.Writer.Write(b)
 }
 
@@ -101,5 +182,8 @@ func generateTLSConfig() *tls.Config {
 	if err != nil {
 		panic(err)
 	}
-	return &tls.Config{Certificates: []tls.Certificate{tlsCert}}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo-example"},
+	}
 }
